8370dee3cffd5de6c1eaef88da2b3235c8b07342
Anita Ganti <65381336+anitapeppercorn@users.noreply.github.com>
2020-08-25 21:33:16 -0700

    image read write on aws s3 bucket

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config(); 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
